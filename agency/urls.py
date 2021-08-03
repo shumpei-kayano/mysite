d3c34805eb7894bd5a975dd2522b51302c23ceb3
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2021-08-03 19:37:30 +0900

    chapter11.P280

--- agency/urls.py
+++ agency/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('photos/',views.PhotosView.as_view(),name="photos"),
     path('about/',views.AboutView.as_view(),name="about"),
     path('diary-list/', views.DiaryListView.as_view(), name="diary_list"),
-    # path('diary-detail/<int:pk>/', views.DiaryDetailView.as_view(), name="diary_detail"),
-    # path('diary-create/', views.DiaryCreateView.as_view(), name="diary_create"),
-    # path('diary-update/<int:pk>/', views.DiaryUpdateView.as_view(), name="diary_update"),
-    # path('diary-delete/<int:pk>/', views.DiaryDeleteView.as_view(), name="diary_delete"),
+    path('diary-detail/<int:pk>/', views.DiaryDetailView.as_view(), name="diary_detail"),
+    path('diary-create/', views.DiaryCreateView.as_view(), name="diary_create"),
+    path('diary-update/<int:pk>/', views.DiaryUpdateView.as_view(), name="diary_update"),
+    path('diary-delete/<int:pk>/', views.DiaryDeleteView.as_view(), name="diary_delete"),
 ]
--- agency/views.py
+++ agency/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render
 # genericの読み込みで使用できるようにする
 from django.views import generic
 # 問い合わせページInquiryFormの読み込み
-from.forms import InquiryForm
+from.forms import InquiryForm, DiaryCreateForm
 from .models import Diary
 from django.contrib import messages
 
@@ -51,3 +51,49 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('agency:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '投稿を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "投稿の作成に失敗しました。")
+        return super().form_invalid(form)
+
+class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Diary
+    template_name = 'diary_update.html'
+    form_class = DiaryCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy('agency:diary_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        messages.success(self.request, '投稿を更新しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "投稿の更新に失敗しました。")
+        return super().form_invalid(form)
+
+class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Diary
+    template_name = 'diary_delete.html'
+    success_url = reverse_lazy('agency:diary_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "投稿を削除しました。")
+        return super().delete(request, *args, **kwargs)
